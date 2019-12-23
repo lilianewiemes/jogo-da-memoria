e530b7f28e5ab59ec4971409fb1c20a76c6d13bb
lilianewiemes <liliane_wiemes@hotmail.com>
2019-12-23 14:57:47 -0300

    Creating funcionality of cards

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -10,10 +10,35 @@ const createMemoryCard = ({ src, alt }) => `
         <article class="card">
             <img 
                 src="img/icon-collabcode.png" 
-                alt=Mascote da Colsbcode" 
+                alt="Mascote da Collabcode" 
                 class="icon"
             />
         </article>
     </div>`
 
-const handleClick = element => element.classList.toggle('-active')
+const handleClick = element => {
+    if (qtdActiveMemoryCard < 2) {
+        element.classList.add('-active')
+    }
+    
+    if (qtdActiveMemoryCard === 1 ) {
+        const activeMemoryCards = document.querySelectorAll('.memory-card.-active')
+
+        if (activeMemoryCards[0].querySelector('.-front .icon').getAttribute('src') ===
+        activeMemoryCards[1].querySelector('.-front .icon').getAttribute('src')) {
+            score++
+            activeMemoryCards.forEach(item => {
+                item.classList.add('-score')
+                item.classList.remove('-active')
+            })
+        } else {
+            setTimeout(() => {
+                activeMemoryCards.forEach(item => {
+                    item.classList.remove('-active')
+                })
+    
+                qtdActiveMemoryCard = 0
+            }, 1000)
+        }
+    }
+}
--- src/objects/cards-wrapper/index.js
+++ src/objects/cards-wrapper/index.js
@@ -1,6 +1,13 @@
+let qtdActiveMemoryCard = 0
+let score = 0
+
 function createCardWrapper() {
     const cardsWrapper = document.createElement('section')
     cardsWrapper.classList.add('cards-wrapper')
     
+    cardsWrapper.addEventListener('click', () => {
+        qtdActiveMemoryCard = cardsWrapper.querySelectorAll('.memory-card.-active').length
+    })
+
     return cardsWrapper
 }
